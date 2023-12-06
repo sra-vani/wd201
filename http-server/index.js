743137b28ed5701962d5cbeea168d1fd5de8ea21
sra-vani <garimellasravani2004@gmail.com>
2023-12-06 15:21:45 +0530

    lint staged

---
+++ http-server/index.js
@@ -0,0 +1,53 @@
+const http = require("http");
+const fs = require("fs");
+
+const args = require("minimist")(process.argv.slice(1));
+
+//console.log(args);
+
+let homeContent = "";
+let projectContent = "";
+let regContent = "";
+
+
+fs.readFile("home.html", (err, home) => {
+  if (err) {
+    throw err;
+  }
+  homeContent = home;
+});
+
+fs.readFile("project.html", (err, project) => {
+  if (err) {
+    throw err;
+  }
+  projectContent = project;
+});
+
+fs.readFile("registration.html", (err, reg) => {
+  if (err) {
+    throw err;
+  }
+  regContent = reg;
+});
+
+http
+  .createServer((request, response) => {
+    let url = request.url;
+    response.writeHeader(200, { "Content-Type": "text/html" });
+    switch (url) {
+      case "/project":
+        response.write(projectContent);
+        response.end();
+        break;
+      case "/registration":
+        response.write(regContent);
+        response.end();
+        break;
+      default:
+        response.write(homeContent);
+        response.end();
+        break;
+    }
+  })
+  .listen(args.port);
---
+++ todo-cli/todo.js
@@ -0,0 +1,55 @@
+/* eslint-disable no-undef */
+const todoList = () => {
+  all = [];
+
+  const add = (todoItem) => {
+    all.push(todoItem);
+  };
+
+  const markAsComplete = (index) => {
+    all[index].completed = true;
+  };
+
+  const overdue = () => {
+    const today = new Date().toISOString().split("T")[0];
+    return all.filter((todo) => !todo.completed && todo.dueDate < today);
+  };
+
+  const dueToday = () => {
+    const today = new Date().toISOString().split("T")[0];
+    return all.filter((todo) => todo.dueDate === today);
+  };
+
+  const dueLater = () => {
+    const today = new Date().toISOString().split("T")[0];
+    return all.filter((todo) => !todo.completed && todo.dueDate > today);
+  };
+
+  const toDisplayableList = (list) => {
+    return list
+      .map((todo) => {
+        const status = todo.completed ? "[x]" : "[ ]";
+        const excludeDueDate = ["pay rent", "service vehicle"];
+        const dueDate =
+          !excludeDueDate.includes(todo.title.toLowerCase()) && todo.dueDate
+            ? " " + formattedDate(new Date(todo.dueDate))
+            : "";
+        return `${status} ${todo.title}${dueDate}`;
+      })
+      .join("\n");
+  };
+  const formattedDate = (d) => {
+    return d.toISOString().split("T")[0];
+  };
+
+  return {
+    all,
+    add,
+    markAsComplete,
+    overdue,
+    dueToday,
+    dueLater,
+    toDisplayableList,
+  };
+};
+module.exports = todoList;
