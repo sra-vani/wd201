6aa4805bac392bb6935b93371a1dc994ba315f0c
sra-vani <139345871+sra-vani@users.noreply.github.com>
2024-01-19 08:43:56 +0530

    Update todo.js

--- todo-app/models/todo.js
+++ todo-app/models/todo.js
@@ -1,5 +1,5 @@
 "use strict";
-const { Model } = require("sequelize");
+const { Model, DataTypes, Op } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class Todo extends Model {
     /**
@@ -19,10 +19,58 @@ module.exports = (sequelize, DataTypes) => {
       return this.update({ completed: true });
     }
 
-    static getTodos = async () => {
+    static async getOverdueTodos() {
+      try {
+        const overdueTodos = await Todo.findAll({
+          where: {
+            dueDate: {
+              [Op.lt]: new Date(),
+            },
+          },
+        });
+        return overdueTodos;
+      } catch (error) {
+        console.error('Error getting overdue todos:', error);
+        throw error;
+      }
+    }
+
+    static async getDueTodayTodos() {
+      try {
+        const dueTodayTodos = await Todo.findAll({
+          where: {
+            dueDate: {
+              [Op.between]: [new Date(), new Date(new Date().setHours(23, 59, 59, 999))],
+            },
+          },
+        });
+        return dueTodayTodos;
+      } catch (error) {
+        console.error('Error getting due today todos:', error);
+        throw error;
+      }
+    }
+
+    static async getDueLaterTodos() {
+      try {
+        const dueLaterTodos = await Todo.findAll({
+          where: {
+            dueDate: {
+              [Op.gt]: new Date(),
+            },
+          },
+        });
+        return dueLaterTodos;
+      } catch (error) {
+        console.error('Error getting due later todos:', error);
+        throw error;
+      }
+    }
+
+    static async getTodos() {
       const todos = await Todo.findAll();
       return todos;
-    };
+    }
   }
   Todo.init(
     {
