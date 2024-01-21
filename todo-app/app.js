cd1bb664dc33fb2f50780d69e425efdf971287cc
sra-vani <139345871+sra-vani@users.noreply.github.com>
2024-01-21 13:42:05 +0530

    Update app.js

--- todo-app/app.js
+++ todo-app/app.js
@@ -3,23 +3,35 @@ const app = express();
 const { Todo } = require("./models");
 const bodyParser = require("body-parser");
 const path = require("path");
+
 app.use(bodyParser.json());
 
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", async (request, response) => {
-  const allTodos = await Todo.getTodos();
-  if (request.accepts("html")) {
-    response.render("index.ejs", {
-      allTodos,
-    });
-  } else {
-    response.json({
-      allTodos,
-    });
+  try {
+    const overdue = await Todo.getOverdueTodos();
+    const duetoday = await Todo.getDueTodayTodos();
+    const duelater = await Todo.getDueLaterTodos();
+
+    if (request.accepts("html")) {
+      response.render("index.ejs", {
+        overdue,
+        duetoday,
+        duelater,
+      });
+    } else {
+      response.json({
+        overdue,
+        duetoday,
+        duelater,
+      });
+    }
+  } catch (error) {
+    console.error(error);
+    response.status(500).json({ error: "Internal Server Error" });
   }
-  response.render("index.ejs");
 });
 
 app.get("/", function (request, response) {
@@ -28,12 +40,13 @@ app.get("/", function (request, response) {
 
 app.get("/todos", async function (_request, response) {
   console.log("Processing list of all Todos ...");
-  // FILL IN YOUR CODE HERE
-  // First, we have to query our PostgerSQL database using Sequelize to get list of all Todos.
-  // Then, we have to respond with all Todos, like:
-  const todos = await Todo.findAll();
-  // response.send(todos)
-  response.send(todos);
+  try {
+    const todos = await Todo.findAll();
+    response.send(todos);
+  } catch (error) {
+    console.error(error);
+    response.status(500).json({ error: "Internal Server Error" });
+  }
 });
 
 app.get("/todos/:id", async function (request, response) {
@@ -41,18 +54,17 @@ app.get("/todos/:id", async function (request, response) {
     const todo = await Todo.findByPk(request.params.id);
     return response.json(todo);
   } catch (error) {
-    console.log(error);
+    console.error(error);
     return response.status(422).json(error);
   }
 });
-
 
 app.post("/todos", async function (request, response) {
   try {
     const todo = await Todo.addTodo(request.body);
     return response.json(todo);
   } catch (error) {
-    console.log(error);
+    console.error(error);
     return response.status(422).json(error);
   }
 });
@@ -63,27 +75,31 @@ app.put("/todos/:id/markAsCompleted", async function (request, response) {
     const updatedTodo = await todo.markAsCompleted();
     return response.json(updatedTodo);
   } catch (error) {
-    console.log(error);
+    console.error(error);
     return response.status(422).json(error);
   }
 });
 
 app.delete("/todos/:id", async function (request, response) {
   console.log("We have to delete a Todo with ID: ", request.params.id);
-  // FILL IN YOUR CODE HERE
-  if (await Todo.findByPk(request.params.id)) {
-    await Todo.destroy({
-      where: {
-        id: request.params.id,
-      },
-    });
+  try {
     if (await Todo.findByPk(request.params.id)) {
-      response.send(false);
+      await Todo.destroy({
+        where: {
+          id: request.params.id,
+        },
+      });
+      if (await Todo.findByPk(request.params.id)) {
+        response.send(false);
+      } else {
+        response.send(true);
+      }
     } else {
-      response.send(true);
+      response.send(false);
     }
-  } else {
-    response.send(false);
+  } catch (error) {
+    console.error(error);
+    response.status(500).json({ error: "Internal Server Error" });
   }
 });
 
