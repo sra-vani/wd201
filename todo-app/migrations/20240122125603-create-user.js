4c308f4331a11e51db7479910635934c69f0138f
sra-vani <139345871+sra-vani@users.noreply.github.com>
2024-01-23 17:01:38 +0530

    Update 20240122125603-create-user.js

--- todo-app/migrations/20240122125603-create-user.js
+++ todo-app/migrations/20240122125603-create-user.js
@@ -16,7 +16,7 @@ module.exports = {
         type: Sequelize.STRING
       },
       email: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
         allowNull: false,
         unique: true,
       },
