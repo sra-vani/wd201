0c465959a3aaaff081234754d6a087a9e416ab7d
sra-vani <139345871+sra-vani@users.noreply.github.com>
2024-01-23 07:44:16 +0530

    Update 20240122132008-add-user-id-in-todos.js

--- todo-app/migrations/20240122132008-add-user-id-in-todos.js
+++ todo-app/migrations/20240122132008-add-user-id-in-todos.js
@@ -1,20 +1,21 @@
-/* eslint-disable no-unused-vars */
-"use strict";
+'use strict';
+
+const { query } = require('express');
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up(queryInterface, Sequelize) {
-    await queryInterface.addColumn("Todos", "userId", {
-      type: Sequelize.DataTypes.INTEGER,
-    });
+  async up (queryInterface, Sequelize) {
+    await queryInterface.addColumn('Todos','userId',{
+      type: Sequelize.DataTypes.INTEGER
+    })
 
-    await queryInterface.addConstraint("Todos", {
-      fields: ["userId"],
-      type: "foreign key",
-      references: {
-        table: "Users",
-        field: "id",
-      },
+    await queryInterface.addConstraint('Todos',{
+      fields:['userId'],
+      type:'foreign key',
+      references:{
+        table:'Users',
+        field:'id'
+      }
     });
     /**
      * Add altering commands here.
@@ -24,13 +25,13 @@ module.exports = {
      */
   },
 
-  async down(queryInterface, Sequelize) {
-    await queryInterface.removeColumn("Todos", "userId");
+  async down (queryInterface, Sequelize) {
+    await queryInterface.renameColumn('Todos','userId')
     /**
      * Add reverting commands here.
      *
      * Example:
      * await queryInterface.dropTable('users');
      */
-  },
+  }
 };
